8bcf871717431f8c3b4fe203243241e9bce4d195
Victor Jatobá <victorjatoba10@gmail.com>
2020-06-08 10:58:28 -0300

    fix: #4 adjust objective logic and another fixies

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,2 @@
-import "./shows";
+import "./objectives";
 import "./user";
--- src/controllers/objectives.ts
+++ src/controllers/objectives.ts
@@ -1,10 +1,5 @@
 import { api } from "../api";
-import { getEpisodes } from "../integrations/tvmaze";
 
-api.fn.getEpisodes = async (ctx, { showId }) => {
-  return (await getEpisodes(ctx, showId)).map(episode => ({
-    id: episode.id,
-    name: episode.name,
-    season: episode.season,
-  }));
+api.fn.getObjectives = async ctx => {
+  return ctx.db.objective.find();
 };
